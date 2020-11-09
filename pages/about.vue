6193b966aa8e3f20b3e689295c3cff4d378f4fcd
Carl Csaposs <carl@csaposs.com>
2020-11-09 12:58:27 -0800

    Add "thank you" section to "about" page (#79)

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <heading heading="About" />
-    <h2>Our Purpose</h2>
+    <h2>Our purpose</h2>
     <p>
       To promote mental health and build community so that everyone can feel
       safe, valued, and understood.
     </p>
-    <h2>Contact Us</h2>
+    <h2>Contact us</h2>
     <p>
       Send us an email at
       <a href="mailto:valueourminds@gmail.com">valueourminds@gmail.com</a>.
@@ -16,12 +16,34 @@
       impacted you, answering your questions, helping you get involved in our
       movement, or hearing from you for another reason.
     </p>
-    <h2>Open Source</h2>
+    <h2>Open source</h2>
     <p>
       The source code for this website is
       <a href="https://github.com/valueourminds/website">available on GitHub</a
       >. Contributions are welcome.
     </p>
+    <h2>Thank you</h2>
+    <p>
+      This movement was made possible by the contributions of the following
+      people (listed in chronological order):
+    </p>
+    <ul>
+      <li>
+        Lucca B, Carl C, Nithmi J, Ellie K, Emily L, Kathy L, Jake R, Kevin R,
+        Nathan S, and Ludi W
+      </li>
+      <li>Scott from Da Vinci High School</li>
+      <li>
+        Shira and the team at the National Coalition for Mental Health Recovery
+      </li>
+      <li>Nance and the team at The Jed Foundation</li>
+      <li>
+        Cara, Jennifer, and Kate from the Davis Joint Unified School District
+      </li>
+      <li>Barbara and Diane from the City of Davis</li>
+      <li>Chris from Netlify</li>
+      <li>Matt and the team at Youth MOVE National</li>
+    </ul>
   </div>
 </template>
 
